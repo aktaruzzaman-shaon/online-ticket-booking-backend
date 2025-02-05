051189557878658faa07211f34009ef2a6859fde
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2025-02-05 21:14:08 +0600

    source change

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const ticketsRouter = require("./routes/ticketRouter")
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/alltickets/', ticketsRouter)
-
 
 module.exports = app
